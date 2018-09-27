6c7cbe2c4cdfb2aa869cfe0c8f2b3dd3a75ee3d9
JoyceAja <ajagbe.joyce@gmail.com>
2018-09-27 19:23:54 -0400

    creating the environment for the app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    data: []
+  }
+
+  componentDidMount = () => {
+    fetch()
+  }
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
