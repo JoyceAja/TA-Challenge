22ca6608bc1527195dd060b42102464c23fe3556
JoyceAja <ajagbe.joyce@gmail.com>
2018-09-27 22:10:31 -0400

    editted the responsiveness

--- src/component/tempPerDay.js
+++ src/component/tempPerDay.js
@@ -5,13 +5,12 @@ export default class extends Component{
         const { data, tempType } = this.props
         var deg = String.fromCharCode(176)
         return(
-        <div>
+        <div className="box">
             <div className="day">
                 <div className="date">{data.dateTimeISO.substring(0, 10)}</div>
                 <div className="icon">{<img src={`/icons/${data.icon}`} alt={data.icon}/>}</div>
                 <div className="maxTemp">{tempType? `Low: ${data.maxTempF} ${deg}F` : `Low: ${data.maxTempC} ${deg}C`}</div>
-                <div className="minTemp">{tempType? `High: ${data.minTempF} ${deg}F` : `High: ${data.minTempC} ${deg}C`}</div>
-                
+                <div className="minTemp">{tempType? `High: ${data.minTempF} ${deg}F` : `High: ${data.minTempC} ${deg}C`}</div>   
             </div>
         </div>
     )
